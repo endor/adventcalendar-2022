2322c153c01b0b348291ab71030b97edb940ad08
Frank Prößdorf <frank@naa.li>
2022-12-06 10:07:05 +0200

    day 06

---
+++ 06/main.jl
@@ -0,0 +1,27 @@
+import Pkg
+
+Pkg.add("DataStructures")
+
+using DataStructures
+
+function start_marker(filename, unique_characters)
+  buffer = CircularBuffer(unique_characters)
+  pos = 1
+
+  open(filename) do f
+    for c in readeach(f, Char)
+      push!(buffer, c)
+
+      if length(buffer) == unique_characters && length(unique(buffer)) == length(buffer)
+        break
+      end
+
+      pos += 1
+    end
+  end
+
+  pos
+end
+
+println(start_marker("input.txt", 4))
+println(start_marker("input.txt", 14))
